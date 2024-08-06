2f2a4b08dc8eed2c9df675ddb2bbb18ae10cec9b
Daniel Bergholz <bergholz.daniel@gmail.com>
2024-08-06 14:46:12 -0300

    Format all files

--- app/root.tsx
+++ app/root.tsx
@@ -3,9 +3,9 @@ import {
   Meta,
   Outlet,
   Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
-import "./tailwind.css";
+  ScrollRestoration
+} from "@remix-run/react"
+import "./tailwind.css"
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -22,9 +22,9 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Scripts />
       </body>
     </html>
-  );
+  )
 }
 
 export default function App() {
-  return <Outlet />;
+  return <Outlet />
 }
